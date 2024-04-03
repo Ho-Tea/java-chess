bf2ddeaca8908657c1e5b8a55f4bc300ba4aaefd
yoonjuho <dbswn990@naver.com>
2024-04-04 01:27:07 +0900

    refactor(view): View Layer 단순하게 재구성

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -15,21 +15,21 @@ public class InputView {
     private static final int TARGET_POSITION_INDEX = 2;
     private static final int POSITION_LENGTH = 2;
     private static final int REQUIRED_TO_MOVE_INPUT_SIZE = 3;
-    private static final int START_OR_END_LENGTH = 1;
 
     private InputView() {
     }
 
     public static GameCommand inputInitialGameCommand() {
         String input = readLine();
-        if (input.split(" ").length != START_OR_END_LENGTH) {
-            throw new IllegalArgumentException("게임 시작(start) 혹은 종료(end)를 입력해 주세요");
-        }
         GameCommand gameCommand = GameCommand.from(input);
-        if (gameCommand == GameCommand.MOVE || gameCommand == GameCommand.STATUS) {
-            throw new IllegalArgumentException("게임을 시작(start)해야 이동 명령을 입력할 수 있습니다.");
-        }
+        validateNotMoveOrStatus(gameCommand);
         return gameCommand;
+    }
+
+    private static void validateNotMoveOrStatus(final GameCommand gameCommand) {
+        if (gameCommand == GameCommand.MOVE || gameCommand == GameCommand.STATUS) {
+            throw new IllegalArgumentException("게임을 시작(start)하거나 불러오기(load)해야 이동 명령을 입력할 수 있습니다.");
+        }
     }
 
     public static GameProceedRequest inputGameProceedCommand() {
@@ -43,30 +43,29 @@ public class InputView {
         return new GameProceedRequest(gameCommand);
     }
 
-    public static GameCommand inputGameStatusCommand() {
-        System.out.println("체스 게임을 종료합니다.");
-        System.out.println("> 게임 현황 확인 : status");
-        System.out.println("> 게임 종료 : end");
-        String input = readLine();
-        GameCommand gameCommand = GameCommand.from(input);
-        if (gameCommand != GameCommand.STATUS && gameCommand != GameCommand.END) {
-            throw new IllegalArgumentException("게임 현황 확인(status) 혹은 종료(end)를 입력해주세요.");
-        }
-        return gameCommand;
-    }
-
-    private static void validateInputSize(List<String> inputMovement) {
+    private static void validateInputSize(final List<String> inputMovement) {
         if (inputMovement.size() != REQUIRED_TO_MOVE_INPUT_SIZE) {
             throw new IllegalArgumentException("source위치 target위치를 입력해 주세요. - 예. move b2 b3");
         }
     }
 
-    private static void validateInputPositionSize(List<String> inputMovement) {
+    private static void validateInputPositionSize(final List<String> inputMovement) {
         String sourcePosition = inputMovement.get(SOURCE_POSITION_INDEX);
         String targetPosition = inputMovement.get(TARGET_POSITION_INDEX);
         if (sourcePosition.length() != POSITION_LENGTH || targetPosition.length() != POSITION_LENGTH) {
             throw new IllegalArgumentException("source위치와 target위치가 올바르지 않습니다.");
         }
+    }
+
+    public static GameCommand inputGameStatusCommand() {
+        System.out.println("체스 게임을 종료합니다.");
+        System.out.println("> 게임 현황 확인 : status");
+        String input = readLine();
+        GameCommand gameCommand = GameCommand.from(input);
+        if (gameCommand != GameCommand.STATUS && gameCommand != GameCommand.END) {
+            throw new IllegalArgumentException("게임 현황 확인(status)을 입력해주세요.");
+        }
+        return gameCommand;
     }
 
     private static String readLine() {
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,6 +1,6 @@
 package view;
 
-import view.dto.PieceInfo;
+import dto.PieceDto;
 
 import java.util.Arrays;
 import java.util.List;
@@ -18,27 +18,24 @@ public class OutputView {
         return chessBoard;
     }
 
-    public void printChessBoard(final List<PieceInfo> pieceInfos) {
+    public void printChessBoard(final List<PieceDto> pieceRespons) {
         char[][] chessBoard = initializeChessBoard();
-        pieceInfos.forEach(pieceInfo -> placePieceOnBoard(chessBoard, pieceInfo));
+        pieceRespons.forEach(pieceInfo -> placePieceOnBoard(chessBoard, pieceInfo));
         printBoard(chessBoard);
     }
 
-    public void printInitialGamePrompt() {
+    public void printGamePrompt() {
         System.out.println("체스 게임을 시작합니다.");
         System.out.println("> 게임 시작 : start");
+        System.out.println("> 게임 불러오기 : load");
         System.out.println("> 게임 종료 : end");
         System.out.println("> 게임 이동 : move source위치 target위치 - 예. move b2 b3");
     }
 
-    public void printAlreadyExistGame(){
-        System.out.println("진행중인 게임을 불러옵니다.");
-    }
-
-    private void placePieceOnBoard(final char[][] chessBoard, final PieceInfo pieceInfo) {
-        int rowIndex = BOARD_SIZE - pieceInfo.rank();
-        int columnIndex = pieceInfo.file() - 1;
-        chessBoard[rowIndex][columnIndex] = pieceInfo.role();
+    private void placePieceOnBoard(final char[][] chessBoard, final PieceDto pieceDto) {
+        int rowIndex = BOARD_SIZE - pieceDto.rank();
+        int columnIndex = pieceDto.file() - 1;
+        chessBoard[rowIndex][columnIndex] = pieceDto.role();
     }
 
     private void printBoard(final char[][] chessBoard) {
@@ -48,13 +45,13 @@ public class OutputView {
         System.out.println();
     }
 
-    public void printScore(double score, String color) {
+    public void printScore(final double score, String color) {
         System.out.printf("%s : %.1f 점", color, score);
         System.out.println();
     }
 
-    public void printWinner(String color) {
-        System.out.println("승리 : " + color);
+    public void printWinner(final String winner) {
+        System.out.println("게임 결과  : " + winner);
     }
 
     public void printExceptionMessage(final String message) {
